1d6b76a281fd86d5fb416a8a1eb45ac263732f26
Shashank <109198739+shashu8660@users.noreply.github.com>
2025-10-07 17:53:34 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from '../components/Hero'
 
-export default function Home() {
+export default function Page() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
